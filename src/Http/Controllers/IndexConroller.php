1c7b60f946889ae04c7eab61e555d69ca5280f20
Sahak <hakobyan.sahak88@gmail.com>
2018-03-13 13:34:08 +0400

    up

--- src/Http/Controllers/IndexConroller.php
+++ src/Http/Controllers/IndexConroller.php
@@ -3,11 +3,16 @@
 namespace BtyBugHook\NewStudio\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\PhpJsonParser;
+use Btybug\Framework\Models\TableCss;
+use Illuminate\Http\Request;
 
 class IndexConroller extends Controller
 {
-    public function getIndex()
-    {
-        return view('newstudio::index');
+    public function getIndex(Request $request, $type = "icons"){
+        $directories = PhpJsonParser::getFoldersWithChildrens();
+        $slug = $request->get('type',$type);
+        $style_from_db = TableCss::where("slug",$slug)->first();
+        return view('newstudio::index', compact(['slug','directories','style_from_db']));
     }
 }
