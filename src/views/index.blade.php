de2dbe9ae943828a143de71e714948c638a46b30
sahakavatar <hakobyan.sahak88@gmail.com>
2018-03-13 15:40:59 +0400

    studio upload form

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -17,7 +17,9 @@
         {{--<h1 class="text-center">Components</h1>--}}
         {{--</div>--}}
         <div class="col-md-3">
-
+            <div class="">
+                @include("newstudio::_partials.left_menu_for_css")
+            </div>
         </div>
         <div class="col-md-9">
             <div class=" headar-btn">
@@ -28,6 +30,15 @@
                     <button class="btn btn-primary show_form_for_setting">Settings</button>
                     <button type="button" class="btn btn-info show_form"><i class="fa fa-plus"></i></button>
                 </div>
+            </div>
+            <div class="show-inp-drop ">
+                 <input type="text" class="form-control">
+                <div class="dropp">
+                    {!! Form::open(['url'=>'/','class'=>'dropzone', 'id'=>'my-awesome-dropzone']) !!}
+
+                    {!! Form::close() !!}
+                </div>
+
             </div>
 
 
@@ -42,6 +53,8 @@
                             <input type="text" id="filename" name="filename" class="form-control" value="{{\App\Http\Controllers\PhpJsonParser::renderName(explode("_",$slug))}}">
                         </div>
                         <div class="clearfix"></div>
+
+                    </div>
                     </div>
                     <div class="form-group">
                         <div class="col-md-4">
@@ -55,12 +68,7 @@
                     </div>
                 </div>
                 <input type="hidden" name="type" value="{{ app('request')->input('type') }}">
-                <div class="col-md-12">
-                    @if($slug != "xl_large_text" && $slug != "l_text" && $slug != "m_text" && $slug != "s_text" && $slug != "xs_text" && $slug != "link_text" && $slug != "icons")
-                        <button type="button" class="btn btn-danger pull-right btn-lg custom_margin_left delete_item_and_classes" data-name="{{$slug}}">Delete this item and its classes</button>
-                    @endif
-                    <button class="btn btn-lg btn-success pull-right">Save</button>
-                </div>
+               
                 {!! Form::close() !!}
             </div>
 
@@ -89,9 +97,7 @@
         </div>
 
         <div class="row layouts_row">
-            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 m-t-54">
-                @include("newstudio::_partials.left_menu_for_css")
-            </div>
+
             <div class="col-xs-12 col-sm-12 col-md-8 col-lg-9">
                 @include("newstudio::_partials.partial_for_css_file")
             </div>
@@ -161,9 +167,139 @@
         .custom_inline_block{
             display:inline-block;
         }
+        .show-inp-drop{
+            display: none;
+        }
+        .show-inp-drop.active{
+            display: block;
+        }
+        .show-inp-drop .form-control{
+            border-radius: 0;
+        }
+        .show-inp-drop .dropp{
+            margin-top: 10px;
+        }
+        .show-inp-drop .dropp #my-awesome-dropzone .dz-default{
+            margin-top: 7% !important;
+        }
     </style>
+    {!! HTML::style('public/js/dropzone/css/dropzone.min.css') !!}
 @stop
 @section('JS')
-    {!! HTML::script('public/js/ace-editor/ace.js') !!}
-    {!! HTML::script('public/js/bty.js?v='.rand(1111,9999)) !!}
+    {{--{!! HTML::script('public/js/bty.js?v='.rand(1111,9999)) !!}--}}
+
+
+
+    {!! HTML::script('public/js/dropzone/js/dropzone.js') !!}
+    <script>
+        $(document).ready(function () {
+            var textarea_editor_for_save = {};
+            var html = $("#get_for_append").html();
+            $(".append_here").html(html);
+            var data = JSON.parse($(".get_data").val());
+            if (html) {
+                var childs = $(".append_here").children("div.class_for_delete").children().children("div.parent").children();
+                if (childs.length) {
+                    childs.map(function (index, item) {
+                        var class_name = data[index].split("{")[0];
+                        class_name = class_name.split(".")[1];
+                        return $(item).addClass(class_name);
+                    });
+                }
+            }
+
+            /*// initialize ace editors
+                data.map(function(item,indx){
+                    if(item.length){
+                        var name = "editor_" + indx;
+                        name = ace.edit("textarea_"+indx);
+                        name.setTheme("ace/theme/monokai");
+                        name.session.setMode("ace/mode/css");
+                        name.setValue(item+"}");
+                    }
+                });
+            // end initialize ace editors*/
+
+            $("body").delegate(".remove_this_class", "click", function () {
+                var slug = $(this).data("slug");
+                var class_name = $(this).data("class");
+                class_name = class_name + "}";
+                var _token = $('input[name=_token]').val();
+                var that = $(this);
+                var url = base_path + "/admin/framework/css-classes/removeclass";
+                $.ajax({
+                    url: url,
+                    data: {
+                        slug: slug,
+                        class_name: class_name,
+                        _token: _token
+                    },
+                    success: function (data) {
+                        if (!data.error) {
+                            return that.parents("div.class_for_delete").remove();
+                        }
+                        return alert("File does not exists");
+                    },
+                    type: 'POST'
+                });
+            });
+            $("body").delegate(".show_in_just_html", "click", function () {
+                var content = $(this).data("class");
+                $("div.just_for_edit").html("");
+                $(this).parents("div.class_for_delete").children("div.just_for_edit").html("<div class='bordered'>" + content + "}" + "</div><div class='clearfix'></div>");
+            });
+
+            $("body").delegate(".show_in_just_html_for_edit", "click", function () {
+                var style = $(this).data("class");
+                var slug = $(this).data("slug");
+
+                var class_name = style.split("{")[0];
+                class_name = class_name.split(".")[1];
+
+                var content = $("#send_form_for_edit").html();
+                content = content.replace("{repl_classname}", "." + class_name).replace("{repl_style}", style + "}").replace("{repl_slug}", slug).replace("{repl_original}", style + "}");
+                $("div.just_for_edit").html("");
+                $(this).parents("div.class_for_delete").children("div.just_for_edit").html(content).find("div.parent").children().addClass(class_name);
+
+                textarea_editor_for_save = ace.edit("textarea_editor_for_save");
+                textarea_editor_for_save.setTheme("ace/theme/monokai");
+                textarea_editor_for_save.session.setMode("ace/mode/css");
+                textarea_editor_for_save.setValue(style + "}");
+                textarea_editor_for_save.on("focus", function () {
+                    textarea_editor_for_save.unsetStyle("set_border");
+                });
+            });
+            $("body").delegate(".check_and_submit", "click", function () {
+                var editor_value = textarea_editor_for_save.getValue();
+                var annot = textarea_editor_for_save.getSession().getAnnotations();
+                for (var key in annot) {
+                    if (annot.hasOwnProperty(key)) {
+                        return textarea_editor_for_save.setStyle("set_border");
+                    }
+                }
+                if (!editor_value) {
+                    return textarea_editor_for_save.setStyle("set_border");
+                }
+                return (
+                    $(".submit_form_for_style_edit").append("<input type='hidden' name='changed_style' value='" + editor_value + "'>").submit()
+                );
+            });
+            $("body").delegate(".custom_cancel", "click", function () {
+                $("div.just_html").html("");
+            });
+          //  $('body').find(".submit_form_for_style").dropzone();
+
+            Dropzone.options.myAwesomeDropzone = {
+                init: function () {
+                    this.on("success", function (file) {
+                        location.reload();
+                    });
+                }
+            };
+            $("body").delegate(".show_form","click",function(){
+
+                    $(".show-inp-drop").toggleClass('active');
+            });
+        });
+    </script>
 @stop
