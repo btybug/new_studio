be37a403c3a785643dad63293d5391af04d7b84c
terakyan <edo.terakyan@gmail.com>
2018-03-14 15:08:46 +0400

    settings

--- src/units/studio_header/settings.blade.php
+++ src/units/studio_header/settings.blade.php
@@ -1,4 +1,29 @@
-<div id="settings_div111">
+<div class="col-md-12">
+    <div class="row  visibility-box">
+        <div class="bty-panel-collapse">
+            <div>
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#inputsetting"
+                   aria-expanded="true">
+                    <span class="icon"><i class="fa fa-chevron-down" aria-hidden="true"></i></span>
+                    <span class="title">General Settings</span>
+                </a>
+            </div>
+            <div id="inputsetting" class="collapse in" aria-expanded="true" style="">
+                <div class="content bty-settings-panel">
+                    <div class="form-group col-md-12">
+                        <div class="col-md-2">
+                            <label>Select studios</label>
+                        </div>
+                        <div class="col-md-9">
+                            {!! Form::select('studios[]',[],null,
+                            ['class' => 'form-control pull-right select-dropdowns','multiple' => 'multiple']) !!}
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 
